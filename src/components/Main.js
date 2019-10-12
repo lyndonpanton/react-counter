b0819273ebcc956494311f16bf7e00de2a41f511
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 16:13:12 +0100

    defined count decrement method

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,14 @@ class Main extends React.Component {
 		};
 	};
 
+	handleDecrement = () => {
+		const count = this.state.count;
+
+		this.setState({
+			count: count - 1
+		});
+	};
+
 	render() {
 		const count = this.state.count;
 
