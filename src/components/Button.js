f4afd1148a19dd460f369b52917b113116e7df58
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 20:27:07 +0100

    added comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const Button = (props) => {
 	return (
+		/* this element gets its styling from the props passed by its container */
+		/* and its "onClick" attribute is actually a plain javascript event lister */
+		/* so when the button is pressed the value (method) of the "handleClick" */
+		/* which was passed to the component is called */
 		<button type={props.type} style={props.styling} onClick={props.handleClick}>{props.icon}</button>
 	);
 };
