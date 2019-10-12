a3ccbb13754a0bf68d51467c882e05870732441b
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 15:21:52 +0100

    added state properties

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,16 @@ import React from "react";
 import Header from "./Header"
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			author: "Lyndon Panton",
+			copyright: "All Rights Reserved",
+			name: "React Counter",
+			year: (new Date()).getFullYear()
+		};
+	};
+
 	render() {
 		return (
 			<div className="app">
