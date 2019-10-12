7ed5e4b3c5c1bde8b19385b8d9a4eadd294058d2
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 19:51:18 +0100

    added comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+// ReactDOM is for working with the browser display
+// the render method is for displaying elements, components
 ReactDOM.render(
-	<App />,
-	document.getElementById("root")
+	<App />, // this component is placed inside...
+	document.getElementById("root") // ...this element
 );
