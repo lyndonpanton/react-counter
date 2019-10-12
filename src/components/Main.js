687750542c81185ef3196f281a1b62b04ae7b0ed
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 15:57:10 +0100

    added count to state which will track count

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 class Main extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0
+		};
+	};
+
 	render() {
 		return (
 			<main>
 
 			</main>
 		);
-	}
+	};
 };
 
 export default Main;
