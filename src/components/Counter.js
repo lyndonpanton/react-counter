1b2049f1538ba10f24269ecaf5ed6308dbd21186
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 20:32:09 +0100

    added comments

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,8 @@ import React from "react";
 const Counter = (props) => {
 	return (
 		<div className="counter">
+			{ /* the count prop is displayed here and is updated when */ }
+			{ /* the state of the component it belongs to updates */ }
 			<p>{props.count}</p>
 		</div>
 	);
