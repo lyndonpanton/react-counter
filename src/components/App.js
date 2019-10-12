95bfc0a9377c17ae4195ea1147d2fc0eddb5cc34
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 15:19:12 +0100

    rendered header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./Header"
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="app">
-				<h1>React Counter</h1>
+				<Header />
 			</div>
 		);
 	};
