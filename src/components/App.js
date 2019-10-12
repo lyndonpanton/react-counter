3b8da46889192b44bbb8e8e7b248e96c2bb4277b
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 15:33:06 +0100

    passed state to header component

--- src/components/App.js
+++ src/components/App.js
@@ -13,9 +13,11 @@ class App extends React.Component {
 	};
 
 	render() {
+		const { name } = this.state;
+
 		return (
 			<div className="app">
-				<Header />
+				<Header name={name} />
 			</div>
 		);
 	};
