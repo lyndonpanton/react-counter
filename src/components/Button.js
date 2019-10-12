cc848858d10a66a2950380de5cf21100c4e28392
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 18:57:11 +0100

    attached event handler to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = (props) => {
 	return (
-		<button>Decrement/Increment</button>
+		<button onClick={props.handleClick}>Decrement/Increment</button>
 	);
 };
 
