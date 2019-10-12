689ffec682734c18c5122d21d2f3a6d0ab96fb2f
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-12 15:46:29 +0100

    defined, applied styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 const Footer = (props) => {
+	const footerStyling = {
+		padding: "80px",
+		backgroundColor: "#F8F8F8",
+		textAlign: "center",
+		color: "#777777"
+	};
+
 	return (
-		<footer>
+		<footer style={footerStyling}>
 			<p>&copy; 2019 Lyndon Panton | All Rights Reserved</p>
 		</footer>
 	);
